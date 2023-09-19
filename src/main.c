02ade1fb2667de513727dbe5c1b845acc474e53d
Gabin PAQUES <106560023+gabpak@users.noreply.github.com>
2023-09-19 22:07:13 +0200

    [v0.0.1-Add] - Map Creation

--- src/main.c
+++ src/main.c
@@ -21,7 +21,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAP_SIZE 10
+
+// ~~~~~~~~~~~~~~~~~~~~~~~ Functions Prototypes ~~~~~~~~~~~~~~~~~~~~~~~ //
+void initMap(int map[][MAP_SIZE]);
+void drawMap(int map[][MAP_SIZE]);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~ Init ~~~~~~~~~~~~~~~~~~~~~~~ //
+typedef enum {false, true} bool;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~ Main ~~~~~~~~~~~~~~~~~~~~~~~ //
 int main(){
-    printf("Hello World!\n");
+    bool isRunning = true;
+    int map[][MAP_SIZE] = { {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},};
+    initMap(map);
+
+    while(isRunning){
+        //TODO: Game loop
+        drawMap(map);
+        break;
+    }
+
     return 0;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~ Functions Definitions ~~~~~~~~~~~~~~~~~~~~~~~ //
+void initMap(int map[][MAP_SIZE]){
+    for(int i = 0; i < MAP_SIZE; i++){
+        for(int j = 0; j < MAP_SIZE; j++){
+            map[i][j] = 0;
+        }
+    }
+}
+
+void drawMap(int map[][MAP_SIZE]){
+    for(int i = 0; i < MAP_SIZE; i++){
+        for(int j = 0; j < MAP_SIZE; j++){
+            if(map[i][j] == 0){
+                printf(". ");
+            }
+            else{
+                printf("%c ", 254u);
+            }
+
+        }
+        printf("\n");
+    }
 }
