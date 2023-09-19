0b838341205e717d3a433e66379e502e31b3d0b5
Gabin PAQUES <106560023+gabpak@users.noreply.github.com>
2023-09-19 21:19:40 +0200

    [v0.0.1-Spec] Project detail. Modification of the hearder in the main.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
+/*
+    Authors: 
+        Gabin PAQUES,
+        Marin DUDOUET,
+        Julia SERPET.
+
+    Created at: 19-09-2023
+
+    Version: 0.0.1
+
+    Description: 
+        Game life project game in C.
+    
+    Compilation:
+        gcc -o main main.c -Wall -Wextra -Werror
+    
+    Execution:
+        ./main
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
