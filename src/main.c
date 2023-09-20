2428ba36d04ed799cafbd9bf5cf16c5f3a515a15
Gabin PAQUES <106560023+gabpak@users.noreply.github.com>
2023-09-20 22:02:41 +0200

    [v0.1.0-Add] The game is now working. No map init for the moment.

--- src/main.c
+++ src/main.c
@@ -6,13 +6,16 @@
 
     Created at: 19-09-2023
 
+    Last modification at: 20-09-2023
+
     Version: 0.0.1
 
     Description: 
         Game life project game in C.
     
     Compilation:
-        gcc -o main main.c -Wall -Wextra -Werror
+        debug:      gcc main.c -o main -Wall -Werror
+        release:    gcc main.c -o main -Wall -Werror -Wextra
     
     Execution:
         ./main
@@ -22,13 +25,15 @@
 #include <stdio.h>
 #include <unistd.h> // Sleep function
 
-#define MAP_SIZE 10
+#define MAP_SIZE 20
 #define DEAD 0
 #define ALIVE 1
-#define SLEEP_TIME 250 // In ms
+#define SLEEP_TIME 1
+#define GENERATION_MAX 5
 
 // ~~~~~~~~~~~~~~~~~~~~~~~ Init ~~~~~~~~~~~~~~~~~~~~~~~ //
 typedef enum {false, true} bool;
+int generation = 0;
 
 // ~~~~~~~~~~~~~~~~~~~~~~~ Functions Prototypes ~~~~~~~~~~~~~~~~~~~~~~~ //
 void wait(int ms); // Function to wait (in ms) because sleep() is in seconds.
@@ -39,32 +44,22 @@ void drawMap(int map[][MAP_SIZE]);
 // ~~~~~~~~~~~~~~~~~~~~~~~ Main ~~~~~~~~~~~~~~~~~~~~~~~ //
 int main(){
     bool isRunning = true;
-    int map[][MAP_SIZE] = { {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},};
-    //initMap(map);
+    int map[MAP_SIZE][MAP_SIZE];
+    initMap(map);
 
-    while(isRunning){
-        //TODO: Game loop
-        updateMap(map);
+    while(isRunning && generation < GENERATION_MAX){
+        generation++;
         drawMap(map);
+        updateMap(map);
         wait(SLEEP_TIME);
-        break;
     }
 
     return 0;
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~ Functions Definitions ~~~~~~~~~~~~~~~~~~~~~~~ //
-void wait(int ms){
-    sleep(ms / 1000);
+void wait(int time){
+    sleep(time);
 }
 
 void initMap(int map[][MAP_SIZE]){
@@ -76,10 +71,72 @@ void initMap(int map[][MAP_SIZE]){
 }
 
 void updateMap(int map[][MAP_SIZE]){
-    // TODO
+
+    int newMap[MAP_SIZE][MAP_SIZE]; // We create a new map to not modify the current one
+    initMap(newMap);
+
+    for(int i = 0; i < MAP_SIZE; i++){
+        for(int j = 0; j < MAP_SIZE; j++){
+            int countNeighbour = 0;
+
+            // We count the number of neighbour
+            if((i > 0) && (map[i - 1][j] == ALIVE)){ // Left cell
+                countNeighbour++;
+            }
+            if((i > 0) && (j > 0) && (map[i - 1][j - 1] == ALIVE)){ // Top Left cell
+                countNeighbour++;
+            }
+            if((j > 0) && (map[i][j - 1] == ALIVE)){ // Top cell
+                countNeighbour++;
+            }
+            if((j > 0) && (i < MAP_SIZE - 1) && (map[i + 1][j - 1] == ALIVE)){ // Top Right cell
+                countNeighbour++;
+            }
+
+            if((i < (MAP_SIZE - 1)) && (map[i + 1][j] == ALIVE)){ // Right cell
+                countNeighbour++;
+            }
+            if((i < (MAP_SIZE - 1)) && (j < (MAP_SIZE - 1)) && (map[i + 1][j + 1] == ALIVE)){ // Bottom Right cell
+                countNeighbour++;
+            }
+            if((j < (MAP_SIZE - 1)) && (map[i][j + 1] == ALIVE)){ // Bottom cell
+                countNeighbour++;
+            }
+            if((i < (MAP_SIZE - 1)) && (j < (MAP_SIZE - 1)) && map[i - 1][j + 1] == ALIVE){ // Bottom Left cell
+                countNeighbour++;
+            }
+
+            // /*\ /*\ /*\ /*\ RULES /*\ /*\ /*\ /*\ //
+
+            // Born
+            if( (map[i][j] == DEAD) && (countNeighbour == 3) ){ 
+                newMap[i][j] = ALIVE;
+            }
+            // Survive
+            if( (map[i][j] == ALIVE) && ((countNeighbour == 2) || (countNeighbour == 3)) ){ 
+                newMap[i][j] = ALIVE; // Stay alive
+            }
+            // Death by isolation
+            if( (map[i][j] == ALIVE) && ((countNeighbour == 0) || (countNeighbour == 1))){
+                newMap[i][j] = DEAD;
+            }
+            // Death by surpopulation
+            if( (map[i][j] == ALIVE) && (countNeighbour >= 3)){
+                newMap[i][j] = DEAD;
+            }
+        }
+    }
+
+    // Copy of newMap into map
+    for(int i = 0; i < MAP_SIZE; i++){
+        for(int j = 0; j < MAP_SIZE; j++){
+            map[i][j] = newMap[i][j];
+        }
+    }
 }
 
 void drawMap(int map[][MAP_SIZE]){
+    printf("\nGen: %i / %i \n", generation, GENERATION_MAX);
     for(int i = 0; i < MAP_SIZE; i++){
         for(int j = 0; j < MAP_SIZE; j++){
             if(map[i][j] == DEAD){
