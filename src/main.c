ec7797cb6153d60ecc8a42110d09a986000ac35e
Gabin PAQUES <106560023+gabpak@users.noreply.github.com>
2023-10-17 23:25:30 +0200

    [v3.1.0] Adding the PDCurses graphisms. Can pause the game and exit.

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 
     Last modification at: 17-10-2023
 
-    Version: 3.0.0
+    Version: 3.1.0
 
     Description: 
         Game life project for the ECE Paris course "Programmation C". Implementation of the graphics with the PDCurses library.
@@ -30,10 +30,9 @@
 #include "curses.h"
 
 #define MAP_SIZE 40 // /!\ MAKE SURE THE .lvl FILE IS THE SAME SIZE /!\ //
-#define MAX_GENERATION 100
 #define DEAD 0
 #define ALIVE 1
-#define SLEEP_TIME 25 // Milliseconds
+#define SLEEP_TIME 10 // Milliseconds
 
 FILE *file = NULL;
 
@@ -41,39 +40,60 @@ FILE *file = NULL;
 int currentGeneration = 0;
 
 // ~~~~~~~~~~~~~~~~~~~~~~~ Functions Prototypes ~~~~~~~~~~~~~~~~~~~~~~~ //
-void wait(int ms); // Function to wait (in ms) because sleep() is in seconds.
 void initMap(int map[][MAP_SIZE]);
 void updateMap(int map[][MAP_SIZE]);
 void drawMap(int map[][MAP_SIZE]);
 void readLevel(int map[][MAP_SIZE]);
+void drawBorder();
 
 // ~~~~~~~~~~~~~~~~~~~~~~~ Main ~~~~~~~~~~~~~~~~~~~~~~~ //
 int main(){
-    int map[MAP_SIZE][MAP_SIZE];
-    initMap(map); // Init the map
 
+    // Init the terminal with PDcurses
+    initscr(); // Init the screen
+    curs_set(0); // Hide the cursor
+    noecho(); // Don't show the input
+    timeout(0); // Don't wait for the user to press a key (getch() function)
+    resize_term(MAP_SIZE + 6, MAP_SIZE + 4); // Resize the terminal
+
+    // Init the map
+    int map[MAP_SIZE][MAP_SIZE];
+    initMap(map);
+    
+    // Read the level
     file = fopen("cells.lvl", "r"); // Open the file
     if(file == NULL){
         printf("\nERROR: main() function => file variable is null\n");
         exit(1);
     }
-
     readLevel(map); // Read the file
-
-    while(currentGeneration < MAX_GENERATION){
+    
+    while(1){
         currentGeneration++;
         drawMap(map);
         updateMap(map);
-        wait(SLEEP_TIME);
+
+        char ch = getch();
+        if(ch == 'p'){
+            while(1){
+                curs_set(1);
+                ch = getch();
+                if(ch == 'p'){
+                    break;
+                }
+            }
+        }
+        if(ch == 'q'){
+            break;
+        }
     }
+
+    endwin();
 
     return 0;
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~ Functions Definitions ~~~~~~~~~~~~~~~~~~~~~~~ //
-void wait(int time){
-    sleep(time);
-}
 
 void initMap(int map[][MAP_SIZE]){
     for(int i = 0; i < MAP_SIZE; ++i){
@@ -148,24 +168,41 @@ void updateMap(int map[][MAP_SIZE]){
 }
 
 void drawMap(int map[][MAP_SIZE]){
-    system("cls");
-    printf("\nGen: %i / %i \n", currentGeneration, MAX_GENERATION);
-
+    clear();
+    drawBorder();
+    mvprintw(1, 1, "Generation: %d", currentGeneration);
     for(int i = 0; i < MAP_SIZE; ++i){
         for(int j = 0; j < MAP_SIZE; ++j){
-            if(map[i][j] == DEAD){
-                printf(". "); 
-            }
-            else if(map[i][j] == ALIVE){
-                printf("%c ", 254u); // 254u is the code for the character "█"
-            }
-            else{
-                printf("\nWARNING: drawmap() function => map value is neither ALIVE or DEAD\n");
-                printf("%c ", 146u); // Random character to show that there is a problem
-            }
-
+            if(map[i][j] == DEAD)
+                mvprintw(i + 3, j + 1, "%c ", 32u); // 32u is the code for the character " "
+            else
+                mvprintw(i + 3, j + 1, "%c ", 248u); // 254u is the code for the character "°"
         }
-        printf("\n");
+    }
+    // Pause button
+    mvprintw(MAP_SIZE + 4, 1, "Press 'p' to pause");
+    mvprintw(MAP_SIZE + 5, 1, "Press 'q' to quit");
+    refresh();
+    usleep(SLEEP_TIME * 1000);
+}
+
+void drawBorder(){
+    // Corners
+    mvprintw(2, 0, "%c", 201u);
+    mvprintw(2, MAP_SIZE + 2, "%c", 187u);
+    mvprintw(MAP_SIZE + 3, 0, "%c", 200u);
+    mvprintw(MAP_SIZE + 3, MAP_SIZE + 2, "%c", 188u);
+
+    // Top and bottom
+    for(int i = 1; i <= MAP_SIZE + 1; ++i){
+        mvprintw(2, i, "%c", 205u);
+        mvprintw(MAP_SIZE + 3, i, "%c", 205u);
+    }
+
+    // Left and right
+    for(int i = 3; i < MAP_SIZE + 3; ++i){
+        mvprintw(i, 0, "%c", 186u);
+        mvprintw(i, MAP_SIZE + 2, "%c", 186u);
     }
 }
 
